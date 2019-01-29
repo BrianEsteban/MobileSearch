fbbda296d0eaac90134464c0446942a0dcdcdd1f
JAVIER <JAVIER@10.203.131.40>
2019-01-29 16:51:56 -0500

    Memorizame

---
+++ src/control/InsertarPlan.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+import javax.swing.JOptionPane;
+import mb_version_3.IncludePanel;
+import entidad.Plan;
+/**
+ *
+ * @author JAVIER
+ */
+public class InsertarPlan {
+    private Plan plan;
+
+    public InsertarPlan(Plan plan) {
+        this.plan = plan;
+    }
+    
+    
+}
